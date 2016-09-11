0df82bd4edb1cd93093587f587141f0a3b7ee7f8
hashimshafiq <hashimshafiq0@gmail.com>
2016-09-11 19:10:56 +0500

    BBC Headline App

--- headlines.py
+++ headlines.py
@@ -1,9 +1,21 @@
 from flask import Flask
-
+import feedparser as fp
 app = Flask(__name__)
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
 @app.route("/")
 def get_news():
-    return "no news is good news"
+    feed = fp.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html
+            <body>
+            <h1>BBC Headlines</h1> <br />
+            <b>{0}</b> <br />
+            <i>{1}</i> <br />
+            <p>{2}</p> <br />
+            </body>
+            </html>""".format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+
 
 if __name__=='__main__':
     app.run()
