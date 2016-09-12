0390ee043de206839a1ccb2344a2874b5b35fed1
hashimshafiq <hashimshafiq0@gmail.com>
2016-09-12 13:13:13 +0500

    jinga templates added

--- headlines.py
+++ headlines.py
@@ -1,22 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 import feedparser as fp
 app = Flask(__name__)
-BBC_FEED = "https://www.geo.tv/rss/1/1.xml"
+RSS_FEED = {'world':'https://www.geo.tv/rss/1/0.xml',
+            'pakistan':'https://www.geo.tv/rss/1/1.xml',
+            'world1':'https://www.geo.tv/rss/1/2.xml',
+            'business':'https://www.geo.tv/rss/1/3.xml',
+            'sports':'https://www.geo.tv/rss/1/4.xml'}
 
-@app.route("/")
-def get_news():
-    feed = fp.parse(BBC_FEED)
+
+
+@app.route("/<publication>")
+def get_news(publication="pakistan"):
+    feed = fp.parse(RSS_FEED[publication])
     first_article = feed['entries'][0]
-    return """<html
-            <body>
-            <h1>GEO NEWS Headlines</h1> <br />
-            <b>{0}</b>  <br />
-            {1} <br />
-            <i>{2}</i> <br />
-            <p></p> <br />
-            </body>
-            </html>""".format(first_article.get("title"),first_article.get("link"),first_article.get("description").encode('ascii','ignore').decode('ascii'))
-
+    return render_template("home.html",title=first_article.get("title"),link=first_article.get("link"),pubDate=first_article.get("pubDate"),description=first_article.get("description"))
 
 if __name__=='__main__':
     app.run()
